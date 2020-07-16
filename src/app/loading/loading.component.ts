51622feff2a13ac0f06f4d74c4732dc2a41e658b
gmorae <gmmartins06@gmail.com>
2020-07-16 01:50:10 -0300

    up

---
+++ src/app/loading/loading.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { LoadingService } from './loading.service';
+import { Subscription } from 'rxjs';
+import { LoadingType } from './loading-type';
+
+@Component({
+  selector: 'app-loading',
+  templateUrl: './loading.component.html',
+  styleUrls: ['./loading.component.css'],
+})
+export class LoadingComponent implements OnInit {
+  loadingSubscription: Subscription;
+  loading$: string = LoadingType.STOPPED;
+
+  constructor(private loadingService: LoadingService) {}
+
+  ngOnInit(): void {
+    this.loadingSubscription = this.loadingService.Loading$.subscribe(
+      (data: string) => {
+        this.loading$ = data;
+      }
+    );
+  }
+}
